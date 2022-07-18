a3ea180849d8bfb1ffdddc86004a209cf6f80df7
Rasel Kazi <67517631+RaselKazi@users.noreply.github.com>
2022-07-19 00:07:22 +0600

    fixd responsive

--- components/CertificateList.jsx
+++ components/CertificateList.jsx
@@ -11,17 +11,17 @@ export default function CertificateList({item}) {
               width={400}
               height={300}
             />
-            <div className=" flex flex-col justify-center items-center py-4 px-6 absolute top-48 left-0 w-full h-full  opacity-0 transition-all duration-500  bg-gradient-to-t from-gray-900  to-gray-900/80 group-hover:top-0 group-hover:opacity-100">
+            <div className=" flex flex-col justify-center items-center p-2 sm:py-4 sm:px-6 absolute top-48 left-0 w-full h-full  opacity-0 transition-all duration-500  bg-gradient-to-t from-gray-900  to-gray-900/80 group-hover:top-0 group-hover:opacity-100">
 
-            <p className=" text-lg sm:text-lg  md:text-xl lg:text-2xl font-bold text-gray-100">
+            <p className=" text-sm sm:text-lg my-2 sm:my-2 md:text-xl lg:text-2xl font-bold text-gray-100">
          {item.title}
         </p>
-        <p className="my-3  text-xs sm:text-xs lg:text-sm text-gray-400 text-center">
+        <p className=" hidden sm:block my-3  text-xs sm:text-xs lg:text-sm text-gray-400 text-center">
         {item.discretion}
         </p>
         <a
             target="_blank"
-            className=" px-3 font-bold text-xl  text-white transition duration-200 rounded-md  bg-gradient-to-t from-pink-800 to-pink-500 hover:scale-110 focus:shadow-outline focus:outline-none shadow-xl shadow-pink-500/40 text-center"
+            className=" px-2 sm:px-3 font-bold text-sm sm:text-xl  text-white transition duration-200 rounded-md  bg-gradient-to-t from-pink-800 to-pink-500 hover:scale-110 focus:shadow-outline focus:outline-none shadow-xl shadow-pink-500/40 text-center"
             href={item.source}
             rel="noreferrer"
           >
--- components/ProjectList.jsx
+++ components/ProjectList.jsx
@@ -35,17 +35,17 @@ export default function ProjectList({ portfolio: portfolio }) {
       />
       <div className=" absolute top-0 left-0 w-full h-full border-sky-400 border-2 rotate-3 group-hover:rotate-0 border-dashed  rounded-xl bg-gray-500/60   backdrop-blur transition-all duration-700"></div>
       <div className=" absolute top-0 left-0 w-full h-full border-sky-500 border-2 rotate-6 group-hover:rotate-0 border-dashed  rounded-xl bg-gray-500/40   backdrop-blur transition-all duration-500"></div>
-      <div className="absolute  z-20 rounded-xl inset-0 flex flex-col justify-center p-5 text-center transition-all duration-700  opacity-0 bg-gray-900 hover:opacity-95 ">
-        <p className="mb-2 text-lg md:text-2xl font-bold text-gray-100">
+      <div className="absolute  z-20 rounded-xl inset-0 flex flex-col justify-center p-2 sm:p-5 text-center transition-all duration-700  opacity-0 bg-gray-900 hover:opacity-95 ">
+        <p className="mb-2 text-base sm:text-lg md:text-2xl font-bold text-gray-100">
           {item.title}
         </p>
-        <p className="mb-2 text-sm md:text-base text-gray-400 ">
+        <p className=" hidden sm:block mb-2 text-sm md:text-base text-gray-400 ">
           {item.type}
         </p>
 
         {
           <div className="flex flex-wrap items-center justify-center mb-4  text-gray-900">
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap h-14 sm:h-auto overflow-hidden">
               <p className="text-lg  md:text-sm lg:text-base text-gray-200 font-bold mr-2">
                 Tech: 
               </p>
